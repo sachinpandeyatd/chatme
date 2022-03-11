f502f2d14a73a25f113c55f49832866064e440b7
sachinpandeyatd <33sachinf@gmail.com>
2022-03-11 22:12:16 +0530

    basic HTML form for signup created

--- index.php
+++ index.php
@@ -5,8 +5,43 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Chat Me</title>
+    <link rel="stylesheet" href="style.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
 </head>
 <body>
-    Hello World, Chat with me, please.
+    <div class="wrapper">
+        <section class="form signup">
+            <header>Chat Me</header>
+            <form action="#">
+                <div class="error-txt">This is an error message!!</div>
+                <div class="name-details">
+                    <div class="field">
+                        <label for="">First Name</label>
+                        <input type="text" placeholder="Sachin">
+                    </div>
+                    <div class="field">
+                        <label for="">Last Name</label>
+                        <input type="text" placeholder="Pandey">
+                    </div>
+                    <div class="field">
+                        <label for="">Email</label>
+                        <input type="email" placeholder="example@domain.com">
+                    </div>
+                    <div class="field">
+                        <label for="">Password</label>
+                        <input type="password" placeholder="%#xhhs72672guiw$%6">
+                    </div>
+                    <div class="field">
+                        <label for="">Image</label>
+                        <input type="file">
+                    </div>
+                    <div class="field">
+                        <input type="submit" value="Submit">
+                    </div>
+                </div>
+            </form>
+            <div class="link">Already a user? <a href="#">Login</a></div>
+        </section>
+    </div>
 </body>
 </html>
