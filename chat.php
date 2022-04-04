9c65899b38a3f9aa7c3eb4ce93b12716e745adb0
sachinpandeyatd <33sachinf@gmail.com>
2022-04-04 20:24:06 +0530

    add send message feature

--- chat.php
+++ chat.php
@@ -126,13 +126,17 @@
                     </div>
                 </div>
             </div>
-            <form action="#" class="typing-area">
-                <input type="text" placeholder="Type a message here....">
+            <form action="#" class="typing-area" autocomplete="off">
+                <input type="text" name="outgoing_id" value="<?php echo $_SESSION['unique_id']; ?>" hidden>
+                <input type="text" name="incoming_id" value="<?php echo $user_id; ?>" hidden>
+                <input type="text" name="message" class="input-field" placeholder="Type a message here....">
                 <button>
                     <i class="fab fa-telegram-plane"></i>
                 </button>
             </form>
         </section>
+
+        <script src="js/chat.js"></script>
     </div>
 </body>
 </html>
---
+++ php/insert-chat.php
@@ -0,0 +1,16 @@
+<?php
+    session_start();
+    if(isset($_SESSION['unique_id'])){
+        include_once "conf.php";
+        $outgoing_id = mysqli_real_escape_string($conn, $_POST['outgoing_id']);
+        $incoming_id = mysqli_real_escape_string($conn, $_POST['incoming_id']);
+        $message = mysqli_real_escape_string($conn, $_POST['message']);
+
+        if(!empty($message)){
+            $sql = mysqli_query($conn, "INSERT INTO messages (incoming_message_id, outgoing_message_id, `message`) VALUES ({$incoming_id}, {$outgoing_id}, '{$message}')") or die();
+        }
+    }else{
+        header("../login.php");
+    }
+
+?>
