6a8ed9fce080b498b104eeffda96fe150ced0904
sachinpandeyatd <33sachinf@gmail.com>
2022-04-05 19:08:33 +0530

    minor features added in search and users list

--- php/users.php
+++ php/users.php
@@ -3,7 +3,7 @@
     session_start();
     include_once "conf.php";
     $outgoing_id = $_SESSION['unique_id'];
-    $sql = mysqli_query($conn, "SELECT * FROM users");
+    $sql = mysqli_query($conn, "SELECT * FROM users WHERE NOT unique_id = {$outgoing_id}");
     $output = "";
 
     if(mysqli_num_rows($sql) == 1){
