1e5a3fb94caba7a7095f9a9df990e87715de1cc7
sachinpandeyatd <33sachinf@gmail.com>
2022-04-05 20:05:23 +0530

    add scroll automatically to bottom feature in chat section

--- js/chat.js
+++ js/chat.js
@@ -14,11 +14,20 @@ sendBtn.onclick = ()=>{
         if(xhr.readyState === XMLHttpRequest.DONE){
             if(xhr.status === 200){
                 inputField.value = "";
+                scrollToBottom();
             }
         }
     }
     let formData = new FormData(form);
     xhr.send(formData);
+}
+
+chatBox.onmouseenter = ()=>{
+    chatBox.classList.add("active");
+}
+
+chatBox.onmouseleave = ()=>{
+    chatBox.classList.remove("active");
 }
 
 setInterval(()=>{
@@ -30,9 +39,16 @@ setInterval(()=>{
             if(xhr.status === 200){
                 let data = xhr.response;
                 chatBox.innerHTML = data;
+                if(!chatBox.classList.contains("active")){
+                    scrollToBottom();
+                }
             }
         }
     }
     let formData = new FormData(form);
     xhr.send(formData);
 }, 500);
+
+function scrollToBottom(){
+    chatBox.scrollTop = chatBox.scrollHeight;
+}
